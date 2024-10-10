0b0a44d9cc94fbbf511e1b08674e53f43983176c
Claudio Lau <claudio.lau12@gmail.com>
2024-10-10 16:55:50 -0400

    update homebrew

---
+++ .config/zsh/.zshrc
@@ -0,0 +1,4 @@
+export PATH="/opt/homebrew/bin:$PATH"
+trap 'source ~/.zshrc' EXIT
+
+
--- install.sh
+++ install.sh
@@ -6,18 +6,18 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
-
 brew bundle install
+stow -t $HOME .config
+source $HOME/.zshenv
 
 ###############################################################################
 # Bin									      #
 ###############################################################################
 
-# Loop through all .sh files in the ./bin directory
 for script in ./bin/*.sh; do
     if [ -f "$script" ]; then
         echo "Executing $script"
-        sudo "$script"  # Execute the script with sudo
+        sudo "$script" 
     fi
 done
 
