26fe09b78faaecd4abf8e5765c7ce643d02b030d
Claudio Lau <claudiolau@claudioacstudio.home>
2024-11-07 08:42:27 -0500

    fix(config): updated configuration scripts

--- set-up.sh
+++ set-up.sh
@@ -7,7 +7,7 @@
 mkdir -p ~/.config
 eval "$(/opt/homebrew/bin/brew shellenv)"
 stow -t $HOME/.config .config
-stow -t $HOME etc
+stow -D -t $HOME etc
 
 ###############################################################################
 # Secrets                                                                     # 
