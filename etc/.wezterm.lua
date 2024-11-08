475b83c1f0bb0195f376db76feb12e7cfa2be8a9
Claudio Lau <claudio.lau12@gmail.com>
2024-11-08 09:23:00 -0500

    fix: update changes
    remove zsh-history and updated wez close terminal

--- etc/.wezterm.lua
+++ etc/.wezterm.lua
@@ -1,12 +1,6 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- my coolnight colorscheme
 config.colors = {
 	foreground = "#CBE0F0",
 	background = "#011423",
@@ -19,14 +13,26 @@ config.colors = {
 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 
+-- ui screen
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 19
 config.enable_tab_bar = false
-
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 10
-
+-- actions
+config.skip_close_confirmation_for_processes_named = {
+	"bash",
+	"sh",
+	"zsh",
+	"fish",
+	"tmux",
+	"nu",
+	"cmd.exe",
+	"pwsh.exe",
+	"powershell.exe",
+	"nvim",
+}
 config.keys = {
 	{
 		key = "w",
