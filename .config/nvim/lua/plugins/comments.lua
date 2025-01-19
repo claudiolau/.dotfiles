d0314e61ee0f075415b5deeb14dec9e4fd49da31
Claudio Lau <claudio.lau12@gmail.com>
2025-01-19 12:36:39 -0500

    fix(startup): resolve startup screen

---
+++ .config/nvim/lua/plugins/comments.lua
@@ -0,0 +1,10 @@
+return {
+  'folke/todo-comments.nvim',
+  dependencies = { "echasnovski/mini.icons", opts = {} },
+  -- TODO:
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+}
