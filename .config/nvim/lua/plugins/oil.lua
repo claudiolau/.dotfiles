784872f12f3c1464ea790515b035921a39f4f0f3
Claudio Lau <claudio.lau12@gmail.com>
2025-01-21 13:24:52 -0500

    fix(lsp): added lsp configuration

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -4,14 +4,8 @@ return {
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
 		config = function()
 			require("oil").setup({
+        default_file_explorer = true,
 				columns = { "icon" },
-				keymaps = {
-					["<C-h>"] = false,
-					["<C-l>"] = false,
-					["<C-k>"] = false,
-					["<C-j>"] = false,
-					["<M-h>"] = "actions.select_split",
-				},
 				view_options = { show_hidden = true },
 			})
 
