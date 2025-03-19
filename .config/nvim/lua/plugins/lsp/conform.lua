454c795d450328944c48b4b18d6534d933b167b9
Claudio Lau <claudio.lau12@gmail.com>
2025-03-19 11:21:31 -0400

    fix(typescript): resolve import organize on save

--- .config/nvim/lua/plugins/lsp/conform.lua
+++ .config/nvim/lua/plugins/lsp/conform.lua
@@ -1,39 +1,70 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  'stevearc/conform.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
   config = function()
-    local conform = require("conform")
+    local conform = require('conform')
 
     conform.setup({
       formatters_by_ft = {
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        svelte = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        graphql = { "prettier" },
-        liquid = { "prettier" },
-        lua = { "stylua" },
-        python = { "isort", "black" },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        svelte = { 'prettier' },
+        css = { 'prettier' },
+        html = { 'prettier' },
+        json = { 'prettier' },
+        yaml = { 'prettier' },
+        markdown = { 'prettier' },
+        graphql = { 'prettier' },
+        liquid = { 'prettier' },
+        lua = { 'stylua' },
+        python = { 'isort', 'black' },
       },
-      format_on_save = {
+      format_after_save = {
         lsp_fallback = true,
-        async = false,
+        async = true,
         timeout_ms = 1000,
       },
     })
 
-    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-      conform.format({
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      })
-    end, { desc = "Format file or range (in visual mode)" })
+    -- Function to organize imports
+    local function organize_imports()
+      local params = {
+        command = '_typescript.organizeImports',
+        arguments = { vim.api.nvim_buf_get_name(0) },
+        title = '',
+      }
+      vim.lsp.buf.execute_command(params)
+    end
+
+    -- LSP setup for TypeScript
+    require('lspconfig').ts_ls.setup {
+      on_attach = function(client, bufnr)
+        -- Automatically organize imports on save
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          buffer = bufnr,
+          callback = function()
+            organize_imports()
+          end,
+        })
+
+        -- Key mapping for organizing imports
+        vim.api.nvim_buf_set_keymap(
+          bufnr,
+          'n',
+          '<leader>oi',
+          '<cmd>lua organize_imports()<CR>',
+          { noremap = true, silent = true, desc = 'Organize Imports' }
+        )
+      end,
+      capabilities = {}, -- Add your capabilities if needed
+      commands = {
+        OrganizeImports = {
+          organize_imports,
+          description = 'Organize Imports',
+        },
+      },
+    }
   end,
 }
