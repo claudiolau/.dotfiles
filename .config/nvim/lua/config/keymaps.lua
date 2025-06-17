95e0af19a6b360213875ebef3fcd0ef3f37c9c8d
Claudio Lau <claudio.lau12@gmail.com>
2025-06-17 14:06:13 -0400

    fix(keymap): updated keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -13,6 +13,8 @@ vim.api.nvim_set_keymap('t', '<leader>q', '<C-\\><C-n>:q!<CR>', { noremap = true
 vim.api.nvim_set_keymap('n', '<S-Tab>', '<C-w>W', { noremap = true, silent = true }) -- Previous window
 vim.api.nvim_set_keymap('n', '<Tab>', '<C-w>w', { noremap = true, silent = true }) -- Next window
 
--- Code Block
+-- Shift Code Block
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true, silent = true, desc = 'Move selected lines down' })
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true, silent = true, desc = 'Move selected lines up' })
+vim.keymap.set('v', '<Tab>', '>gv', { noremap = true, silent = true, desc = 'Indent selected lines' })
+vim.keymap.set('v', '<S-Tab>', '<gv', { noremap = true, silent = true, desc = 'Unindent selected lines' })
