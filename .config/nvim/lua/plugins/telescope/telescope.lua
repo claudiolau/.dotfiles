40249cdec3d483fa91f17eb84dd67a9f21266093
Claudio Lau <claudio.lau12@gmail.com>
2025-07-03 13:18:29 -0400

    fix(plugins): remove more plugins

--- .config/nvim/lua/plugins/telescope/telescope.lua
+++ .config/nvim/lua/plugins/telescope/telescope.lua
@@ -5,22 +5,10 @@ return {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     { 'echasnovski/mini.icons', opts = {} },
-    'folke/todo-comments.nvim',
   },
   config = function()
     local telescope = require('telescope')
     local actions = require('telescope.actions')
-    local transform_mod = require('telescope.actions.mt').transform_mod
-
-    local trouble = require('trouble')
-    local trouble_telescope = require('trouble.sources.telescope')
-
-    -- or create your custom action
-    local custom_actions = transform_mod({
-      open_trouble_qflist = function(prompt_bufnr)
-        trouble.toggle('quickfix')
-      end,
-    })
 
     telescope.setup({
       defaults = {
@@ -29,8 +17,6 @@ return {
           i = {
             ['<C-k>'] = actions.move_selection_previous, -- move to prev result
             ['<C-j>'] = actions.move_selection_next, -- move to next result
-            ['<C-q>'] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            ['<C-t>'] = trouble_telescope.open,
             ['<C-d>'] = actions.delete_buffer,
           },
           n = {
@@ -52,8 +38,6 @@ return {
     keymap.set('n', '<leader>fs', '<cmd>Telescope live_grep<cr>', { desc = 'Find string in cwd' })
     keymap.set('n', '<leader>fc', '<cmd>Telescope grep_string<cr>', { desc = 'Find string under cursor in cwd' })
     keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'Find buffers' })
-    -- todo
-    keymap.set('n', '<leader>ft', '<cmd>TodoTelescope<cr>', { desc = 'Find todos' })
     -- git
     keymap.set('n', '<leader>fgc', '<cmd>Telescope git_commits<cr>', { desc = 'Find git commit' })
     keymap.set('n', '<leader>fgb', '<cmd>Telescope git_branches<cr>', { desc = 'Find git branches' })
